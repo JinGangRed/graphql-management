1c5663c06e2ad04b4e5439427a36f07526d7649c
JinGangRed <k15950590137@126.com>
2020-01-07 22:14:29 +0800

    Add Util comments

--- Utilities/Strings/ChineseCharacter.cs
+++ Utilities/Strings/ChineseCharacter.cs
@@ -7,6 +7,10 @@ namespace Utilities.Strings
 {
     public static partial class StringExtensions
     {
+        /// <summary>
+        /// 生成中文字
+        /// </summary>
+        /// <returns></returns>
         private static string GenerateChineseWord()
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
@@ -18,13 +22,22 @@ namespace Utilities.Strings
             var bytesCode = new byte[] { (byte)(regionCode + 160), (byte)(positionCode + 160) };
             return Encoding.GetEncoding("GB2312").GetString(bytesCode);
         }
-
-        public static string GenerateChineseName(bool hasSplit = true)
+        /// <summary>
+        /// 生成中文名字
+        /// </summary>
+        /// <param name="hasSplit">是否在姓和名之间添加间隔,默认不添加间隔</param>
+        /// <returns></returns>
+        public static string GenerateChineseName(bool hasSplit = false)
         {
             var random = new Random().Next(1,3);
             return string.Concat(GetRandomSurname(), hasSplit?" ":"", GenerateChineseWord(random).Trim());
         }
-        public static string GenerateChineseWord(int num)
+        /// <summary>
+        /// 生成中文字
+        /// </summary>
+        /// <param name="num">生成中文字的个数,默认只生成一个</param>
+        /// <returns></returns>
+        public static string GenerateChineseWord(int num = 1)
         {
             var words = "";
             for (int i = 0; i < num; i++)
